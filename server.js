605ba47d1bd5b77b8ecdccdf107a407eaa848578
Ajanaku-Olamiposi <olamiposajanaku53@gmail.com>
2025-04-10 08:13:19 +0100

    hi

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(async (err, req, res, next) => {
  * Values from .env (environment) file
  *************************/
 const port = process.env.PORT || 5500
-const host = process.env.HOST
+const host = process.env.HOST || 'local host'
 /* ***********************
  * Log statement to confirm server operation
  *************************/
